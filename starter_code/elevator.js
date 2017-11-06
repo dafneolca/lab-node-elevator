ba40ecf61d78c3e875ad89b18715757cf7d4e8ea
Dafne <olca.dafne@gmail.com>
2017-11-06 21:21:44 +0100

    iteration 1 complete

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,11 +3,25 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this._startInterval = null;
+
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {  //SEEMS OK
+    this._startInterval = setInterval(()=> {
+      this.update();
+    }, 1000);
+  }
+
+  stop() { //SEEMS OK
+    set.timeOut();
+  }
+
+// MAYBE ALMOST DONE (?)
+  update(floor) {
+    console.log("Direction: " + this.requests + " | Floor: " + this.floor);
+  }
+
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
@@ -15,5 +29,8 @@ class Elevator {
   call() { }
   log() { }
 }
+
+
+// Remember to use JavaScript functions such as setTimeout() to create the listener
 
 module.exports = Elevator;
